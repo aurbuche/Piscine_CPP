fcf10ad38520e0db4fd27412c9d2810742abd786
Aurelien Bucher <aurbuche@z2r11p6.42lyon.fr>
2021-06-04 10:45:08 +0200

    CPP06 end

--- CPP06/ex01/main.cpp
+++ CPP06/ex01/main.cpp
@@ -1,9 +1,49 @@
 #include <iostream>
 
-void	*serialize(void)
+struct Data
 {
+	std::string s1;
+	int n;
+	std::string s2;
+};
 
+void *serialize(void)
+{
+	unsigned char *raw = new unsigned char[20];
+
+	// Init first string with printable-only values.
+	for (size_t i = 0; i < 8; ++i)
+		raw[i] = rand() % 26 + 97;
+	// Init integer.
+	for (size_t i = 8; i < 12; ++i)
+		raw[i] = rand() % INT_MAX;
+	// Init second string like first one
+	for (size_t i = 12; i < 20; ++i)
+		raw[i] = rand() % 26 + 97;
+
+	return raw;
 }
 
-int 	main(void)
-{}
+Data *deserialize(void *raw)
+{
+	char *ptr = static_cast<char *>(raw);
+	Data *data = new Data;
+
+	for (size_t i = 0; i < 8; ++i)
+		data->s1 += ptr[i];
+	data->n = *reinterpret_cast<int *>(ptr + 8);
+	for (size_t i = 12; i < 20; ++i)
+		data->s2 += ptr[i];
+
+	return data;
+}
+
+int main(void)
+{
+	srand(time(NULL));
+	void *raw = serialize();
+	Data *d = deserialize(raw);
+
+	std::cout << "(\"" << d->s1 << "\", " << d->n << ", \"" << d->s2 << "\")" << std::endl;
+	return 0;
+}
--- CPP06/ex02/main.cpp
+++ CPP06/ex02/main.cpp
@@ -1,8 +1,77 @@
-#include "Base.hpp"
+#include <iostream>
+#include <time.h>
+
+class Base
+{
+	public:
+		virtual ~Base(){}
+};
+
+class A : public Base
+{
+};
+
+class B : public Base
+{
+};
+
+class C : public Base
+{
+};
+
+Base*	generate() {
+	int irand = rand() % 3;
+	if (irand == 0)
+		return (new A);
+	else if (irand == 1)
+		return (new B);
+	else if (irand == 2)
+		return (new C);
+	return (NULL);
+}
+
+void	identify_from_pointer(Base *p) {
+	if (dynamic_cast<A*>(p))
+		std::cout << "A\n";
+	else if (dynamic_cast<B*>(p))
+		std::cout << "B\n";
+	else if (dynamic_cast<C*>(p))
+		std::cout << "C\n";
+}
+
+void	identify_from_reference(Base & p)
+{
+	try
+	{
+		A &a = dynamic_cast<A&>(p);
+		(void)a;
+		std::cout << "A\n";
+	}
+	catch (std::bad_cast &e)
+	{}
+	try
+	{
+		B &b = dynamic_cast<B&>(p);
+		(void)b;
+		std::cout << "B\n";
+	}
+	catch (std::bad_cast &e)
+	{}
+	try
+	{
+		C &c = dynamic_cast<C&>(p);
+		(void)c;
+		std::cout << "C\n";
+	}
+	catch (std::bad_cast &e)
+	{}
+}
 
 int main(void)
 {
 	srand(time(NULL));
-	Base base;
-	base.generate();
+	Base *base = generate();
+	identify_from_pointer(base);
+	identify_from_reference(*base);
+	delete base;
 }
