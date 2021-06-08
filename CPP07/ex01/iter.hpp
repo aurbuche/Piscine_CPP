87433eae09ab408a3e6ce4b0747d535a5ba36421
Aurelien Bucher <aurbuche@z2r11p6.42lyon.fr>
2021-06-08 13:16:40 +0200

    Makefile corrected

--- CPP07/ex01/iter.hpp
+++ CPP07/ex01/iter.hpp
@@ -1,8 +1,18 @@
 #ifndef ITER_HPP
 # define ITER_HPP
 
-template <typename T>
-void	iter(T const *array, int length, void (*func)(T const &))
+# include <iostream>
+# include <typeinfo>
+# include <string>
+template <typename T, typename S>
+void	iter(T *array, S length, void (*func)(T &))
+{
+	for (int i = 0; i < length; i++)
+		func((array)[i]);
+}
+
+template <typename T, typename S>
+void	iter(T const *array, S length, void (*func)(T const &))
 {
 	for (int i = 0; i < length; i++)
 		func((array)[i]);
--- CPP07/ex01/main.cpp
+++ CPP07/ex01/main.cpp
@@ -1,12 +1,19 @@
 #include "iter.hpp"
 
-#include <iostream>
-#include <string>
+template <typename T>
+void	print(T const & src)
+{
+	std::cout << src;
+}
 
 int main(void)
 {
-	std::string str = "salut a toi";
-	//int test[5] = {15, 16 ,17 ,18 ,19};
-	::iter(str, 5, str.length());
-	std::cout << str << "\n";
+	std::string	str[] = {"Salut ", "petite ", "perruche."};
+	int tab_num[] = {0, 1, 2, 3, 4, 5};
+
+	::iter(tab_num, 6, print);
+	std::cout << std::endl;
+	::iter(str, 3, print);
+	std::cout << std::endl;
+	return (0);
 }
